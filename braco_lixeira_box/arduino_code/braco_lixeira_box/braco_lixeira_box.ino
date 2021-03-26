d0b6185b9d683894aa5de4a265cf5f64783a75f2
cunhamurilo <murilo_rcc@hotmail.com>
2021-03-26 14:09:44 -0300

    Arm trash gmail box update with reset buton

--- braco_lixeira_box/arduino_code/braco_lixeira_box/braco_lixeira_box.ino
+++ braco_lixeira_box/arduino_code/braco_lixeira_box/braco_lixeira_box.ino
@@ -11,6 +11,8 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 SoftwareSerial esp8266(2, 3); // objeto para comunicação serial com o ESP8266 nas Portas 2(RX) e 3(TX)
 
 int RECV_PIN = 0;
+int RESET_PIN = 1;
+
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 float armazenavalor;
@@ -18,7 +20,7 @@ float armazenavalor;
 const int stepsPerRevolution = 500;
 Stepper stepperLefRight(stepsPerRevolution, 4, 6, 5, 7);
 Stepper stepperUpDown(stepsPerRevolution, 8, 10, 9, 11);
-Stepper stepperFrontBack(stepsPerRevolution, A3, A1, A2, A0);
+Stepper stepperFrontBack(stepsPerRevolution, A3, A1, A2, A0);  // voltar para A0
 
 #define pino_trigger 13
 #define pino_echo 12
@@ -29,10 +31,13 @@ unsigned long millisEmail = 0;
 bool near = false;
 
 void setup() {
-  Serial.begin(115200); // inicia a comunicação serial com o monitor
+//  /Serial.begin(115200); // inicia a comunicação serial com o monitor
   esp8266.begin(115200); // inicia a comunicação serial com o ESP8266
 
   irrecv.enableIRIn(); // Inicializa o receptor IR
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(RESET_PIN, INPUT);
 
   stepperUpDown.setSpeed(60);
   stepperLefRight.setSpeed(60);
@@ -42,6 +47,7 @@ void setup() {
   pwm.setOscillatorFrequency(27000000);  // The int.osc. is closer to 27MHz
   pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
 
+  pwm.setPWM(15, 0, 300);
 }
 
 void loop() {
@@ -50,13 +56,20 @@ void loop() {
   check_distance();
   check_serial();
 
+  if(digitalRead(RESET_PIN)){
+    esp8266.write("reset");
+    digitalWrite(LED_BUILTIN, HIGH);
+  }else{
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
   delay(50);
 }
 
 void check_Ir() {
   if (irrecv.decode(&results)) {
-    Serial.print("Valor lido : ");
-    Serial.println(results.value, HEX);
+//    Serial.print("Valor lido : ");
+//    Serial.println(results.value, HEX);
     armazenavalor = (results.value);
     // Check if up
     if (armazenavalor == 0xFF18E7) {
@@ -100,17 +113,17 @@ void check_distance() {
 
   if (near == false) {
     if (cmMsec < 15) {
-      Serial.println("Menor 15 cm");
+//      Serial.println("Menor 15 cm");
       pwm.setPWM(0, 0, 150);
       millisTarefa1 = millis();
       near = true;
     } else {
-      Serial.print("nao proximo  ");
-      Serial.println(cmMsec);
+//      Serial.print("nao proximo  ");
+//      Serial.println(cmMsec);
     }
   } else {
     if ((millis() - millisTarefa1) > 5000) {
-      Serial.println("5 segundos");
+//      Serial.println("5 segundos");
       pwm.setPWM(0, 0, 300);
       near = false;
       millisTarefa1 = 0;
@@ -122,16 +135,16 @@ void check_distance() {
 void check_serial() {
   if (esp8266.available()) {
     String resp = "";
-    while (esp8266.available())
-    {
+    while (esp8266.available()){
       // The esp has data so display its output to the serial window
       char c = esp8266.read(); // read the next character.
       resp += c;
     }
 
-    Serial.println(resp);
+//    Serial.println(resp);
     if (resp.compareTo("new_email")) {
       pwm.setPWM(15, 0, 150);
+      pwm.setPWM(14, 0, 150);
       millisEmail = millis();
     }
   }
@@ -147,11 +160,13 @@ void setServoPulse(uint8_t n, double pulse) {
 
   pulselength = 1000000;   // 1,000,000 us per second
   pulselength /= SERVO_FREQ;   // Analog servos run at ~60 Hz updates
-  Serial.print(pulselength); Serial.println(" us per period");
+//  Serial.print(pulselength); 
+//  Serial.println(" us per period");
   pulselength /= 4096;  // 12 bits of resolution
-  Serial.print(pulselength); Serial.println(" us per bit");
+//  Serial.print(pulselength); 
+//  Serial.println(" us per bit");
   pulse *= 1000000;  // convert input seconds to us
   pulse /= pulselength;
-  Serial.println(pulse);
+//  Serial.println(pulse);
   pwm.setPWM(n, 0, pulse);
 }
--- braco_lixeira_box/arduino_code/gmail/gmail.ino
+++ braco_lixeira_box/arduino_code/gmail/gmail.ino
@@ -1,44 +1,124 @@
 // Bibliotecas utilizadas
 #include <AdafruitIO_WiFi.h>
 #include <ESP8266WiFi.h>
+#include <WiFiManager.h>
  
 // Credenciais da conta Adafruit IO
-#define IO_USERNAME     "your username"
-#define IO_KEY          "your io_key"
+#define IO_USERNAME     "your username adafruit io"
+#define IO_KEY          "your key adafruit io"
  
 // Credenciais da rede WiFi
-#define WIFI_SSID       "your ssid"
 #define WIFI_PASS       "your password"
+String getNameSSID = WiFi.SSID();
+const char *WIFI_SSID = getNameSSID.c_str(); // convert String to char
  
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
  
 AdafruitIO_Feed *gmail_feed = io.feed("gmail");
  
+WiFiManager wifiManager;  //Objeto de manipulação do wi-fi
+
+String incomingByte = "";
+
 void setup() {
    
+  delay(2000);
   Serial.begin(115200);
-  Serial.print("Notificador de e-mails do Gmail"); 
- 
-  // Conexão inicial com a Adafruit IO
-  Serial.print("Conectando com Adafruit IO");
-  io.connect();
-  gmail_feed->onMessage(handleMessage);
-  while(io.status() < AIO_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
+  Serial.println("Notificador de e-mails do Gmail"); 
+
+  if (WiFi.SSID() != "") {
+    // Conexão inicial com a Adafruit IO
+    connectWiFi();
+    Serial.println();
    
-  Serial.println();
- 
-  gmail_feed->get();
+    gmail_feed->get();
+  }else{
+    Serial.print("Sem rede");
+  }
    
 }
  
 void loop() {
-   io.run();
+   resetClick();
+  
+   if(WiFi.status() == WL_CONNECTED){
+      Serial.println("io run");
+      io.run();
+   }else{
+      if(WiFi.SSID() != ""){
+        connectWiFi();
+      }
+   }
+   delay(10);
 }
  
 // Função chamada sempre que há uma novo dado na Adafruit IO
 void handleMessage(AdafruitIO_Data *data) {
   Serial.write("new_email");
+}
+
+void connectWiFi(){
+  Serial.println("Conectando com Adafruit IO");
+  io.connect();
+  gmail_feed->onMessage(handleMessage);
+  while(io.status() < AIO_CONNECTED) {
+    resetClick();
+    delay ( 400 );
+    Serial.print ( "." );
+  }
+  Serial.println("Conectado ao Adafruit IO");
+  
+}
+
+void resetClick(){
+    if (Serial.available() > 0) {
+      // read the incoming byte:
+      incomingByte = "";
+      while (Serial.available()){
+        char c = Serial.read();
+        incomingByte += c;
+      }
+      Serial.println(incomingByte);
+      
+      if (incomingByte.length() > 0) {
+        if(incomingByte.compareTo("reset") == 0){
+          Serial.println("Abertura Portal"); //Abre o portal
+          wifiManager.resetSettings();       //Apaga rede salva anteriormente
+          delay(20);
+            
+          //callback para quando entra em modo de configuração AP
+          wifiManager.setAPCallback(configModeCallback); 
+          //callback para quando se conecta em uma rede, ou seja, quando passa a trabalhar em modo estação
+          wifiManager.setSaveConfigCallback(saveConfigCallback); 
+            
+          if(!wifiManager.autoConnect("ESP_TRASH", "12345678") ){ //Nome da Rede e Senha gerada pela ESP
+            
+            Serial.println("Falha ao conectar"); //Se caso não conectar na rede mostra mensagem de falha
+            delay(2000);
+            wifiManager.resetSettings();  
+            ESP.reset(); //Reinicia ESP após não conseguir conexão na rede
+            
+          }else{       //Se caso conectar 
+            Serial.println("Conectado na Rede!!!");
+            
+            delay(1000);
+           
+            ESP.restart(); //Reinicia ESP após conseguir conexão na rede 
+          }  
+        }
+          
+      }
+    }
+}
+
+//callback que indica que o ESP entrou no modo AP
+void configModeCallback (WiFiManager *myWiFiManager) {  
+  Serial.println("Entrou no modo de configuração");
+  Serial.println(WiFi.softAPIP()); //imprime o IP do AP
+  Serial.println(myWiFiManager->getConfigPortalSSID()); //imprime o SSID criado da rede
+}
+ 
+//Callback que indica que salvamos uma nova rede para se conectar (modo estação)
+void saveConfigCallback () {
+  Serial.println("Configuração salva");
 }
